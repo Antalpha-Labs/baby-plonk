10e6fec8d9e4171e1d437a69880878f09e2f24a8
Harry <2411mail@gmail.com>
2024-06-16 10:29:30 +0800

    homework for SRS

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ class Setup(object):
 
     @classmethod
     # tau: a random number whatever you choose
+    # powers: length of SRS(structured reference string)
     def generate_srs(cls, powers: int, tau: int):
         print("Start to generate structured reference string")
 
@@ -27,10 +28,7 @@ class Setup(object):
         # powers_of_x[2] =  b.G1 * tau**2 = powers_of_x[1] * tau
         # ...
         # powers_of_x[i] =  b.G1 * tau**i = powers_of_x[i - 1] * tau
-        powers_of_x[0] = b.G1
-
-        for i in range(1, powers):
-            powers_of_x[i] = b.multiply(powers_of_x[i - 1], tau)
+        # TODO: generate powers_of_x
 
         print("Generated G1 side, X^1 point: {}".format(powers_of_x[1]))
 
